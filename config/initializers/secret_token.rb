d0528d589b98edb8cbed262ebe96593b6eecc166
gaurav gupta <1989.gaurav@googlemail.com>
2013-04-01 16:14:22 +0530

    Adding auto generated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = 'c6f9e58e1c73b59d3cc91ea4331a49c687fb1286a2ab9dd06af21cb6f7505907f474ffa435658b3a2a4be61f6cbfe61f368ed26807102824321d2c452196d414'
