3b8193677517b16fe9737267408f8e8c8722d05c
gaurav gupta <1989.gaurav@googlemail.com>
2013-04-04 20:28:38 +0530

    Adding validation and fixing functional tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image, :price, :title
+
+  validates :description, :image, :price, :title, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image, :format => {
+      :with => %r{\.(gif|png|jpg)}i,
+      :message => 'must be a URL for GIF, PNG or JPG'
+  }
 end
